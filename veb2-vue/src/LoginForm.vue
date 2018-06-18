3dde653ba70b9a8bf066de3ec97a7ebbbd75e5cf
Grzegorz <gjahn100@gmail.com>
2018-06-18 23:37:36 +0200

    Final

--- veb2-vue/src/LoginForm.vue
+++ veb2-vue/src/LoginForm.vue
@@ -2,7 +2,7 @@
    <div>
        <label>Podaj e-mail:</label>
        <input type="email" v-model="email">
-      
+    <!-- Sprawdzenie emaila-->
        <div v-if="email.match(/^[A-Za-z0-9\-_]+@[A-Za-z0-9\-_\.]+\.pl$/) ">Email prawidołwy</div>
        <div v-else="">Podaj prawidołwy e-mail na polskiej poczcie</div>
        <button @click="enter()">Zaloguj</button>
